c6f77333e5beaf228f076fd897915997b63994c5
ahihi <bdsthocu20@gmail.com>
2023-09-01 23:07:15 +0700

    #9

--- main.go
+++ main.go
@@ -43,6 +43,26 @@ func main() {
 	}
 
 	r := gin.Default()
+
+	//CRUD: Create, Read, Update, Delete
+	//POST: /v1/items (create a new item)
+	//GET: /v1/items (list items) /v1/items?page=1
+	//GET: /v1/items/:id (get item by id)
+	//(PUT || PATCH) /v1/items/:id (update an item by id)
+	//DELETE /v1/items/:id (delete an item by id)
+
+	v1 := r.Group("/v1")
+	{
+		items := v1.Group("/items")
+		{
+			items.POST("")
+			items.GET("")
+			items.GET("/:id")
+			items.PATCH("/:id")
+			items.DELETE("/:id")
+		}
+	}
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": item,
